db01189240ae13a3f68bd922576845a8214d45d1
kamal <kamal.maity@webskitters.com>
2019-01-17 21:04:02 +0530

    bg remover

--- app/src/main/java/com/imagecroper/ResultActivity.java
+++ app/src/main/java/com/imagecroper/ResultActivity.java
@@ -12,41 +12,145 @@
 
 package com.imagecroper;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
+import com.alexvasilkov.gestures.views.interfaces.GestureView;
+import com.imagecroper.bgremover.BitmapUtility;
+import com.imagecroper.bgremover.CutOut;
+import com.imagecroper.bgremover.DrawView;
+import com.imagecroper.bgremover.SaveDrawingTask;
 import com.imagecroper.cropImage.CropImage;
 import com.imagecroper.cropImage.CropImageView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class ResultActivity extends AppCompatActivity {
+import top.defaults.checkerboarddrawable.CheckerboardDrawable;
 
-  private RecyclerView mRecyclerView;
-  private ArrayList<ImageUriList> lists;
-private ImageCropedAdapter adapter;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+public class ResultActivity extends AppCompatActivity {
+  private static final int WRITE_EXTERNAL_STORAGE_CODE = 1;
+  private static final int IMAGE_CHOOSER_REQUEST_CODE = 2;
+  private static final int CAMERA_REQUEST_CODE = 3;
+
+  private static final String INTRO_SHOWN = "INTRO_SHOWN";
+  public FrameLayout loadingModal;
+  private GestureView gestureView;
+  private DrawView drawView;
+  private LinearLayout manualClearSettingsLayout;
+
+  private static final short MAX_ERASER_SIZE = 150;
+  private static final short BORDER_SIZE = 45;
+  private static final float MAX_ZOOM = 4F;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_results);
-    mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-    lists=new ArrayList<>();
-    // use this setting to improve performance if you know that changes
-    // in content do not change the layout size of the RecyclerView
-    mRecyclerView.setHasFixedSize(true);
-    // use a linear layout manager
-    GridLayoutManager manager = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
-    mRecyclerView.setLayoutManager(manager);
-    adapter=new ImageCropedAdapter(lists);
-    mRecyclerView.setAdapter(adapter);
+    /*Toolbar toolbar = findViewById(R.id.photo_edit_toolbar);
+    toolbar.setBackgroundColor(Color.BLACK);
+    toolbar.setTitleTextColor(Color.WHITE);
+    setSupportActionBar(toolbar);*/
 
+    FrameLayout drawViewLayout = findViewById(R.id.drawViewLayout);
+
+    int sdk = android.os.Build.VERSION.SDK_INT;
+
+    if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+      drawViewLayout.setBackgroundDrawable(CheckerboardDrawable.create());
+    } else {
+      drawViewLayout.setBackground(CheckerboardDrawable.create());
+    }
+
+    SeekBar strokeBar = findViewById(R.id.strokeBar);
+    strokeBar.setMax(MAX_ERASER_SIZE);
+    strokeBar.setProgress(50);
+
+    gestureView = findViewById(R.id.gestureView);
+
+    drawView = findViewById(R.id.drawView);
+    drawView.setDrawingCacheEnabled(true);
+    drawView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+    //drawView.setDrawingCacheEnabled(true);
+    drawView.setStrokeWidth(strokeBar.getProgress());
+
+    strokeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        drawView.setStrokeWidth(seekBar.getProgress());
+      }
+    });
+
+    loadingModal = findViewById(R.id.loadingModal);
+    loadingModal.setVisibility(INVISIBLE);
+
+    drawView.setLoadingModal(loadingModal);
+
+    manualClearSettingsLayout = findViewById(R.id.manual_clear_settings_layout);
+
+    setUndoRedo();
+    initializeActionButtons();
+
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      getSupportActionBar().setDisplayShowHomeEnabled(true);
+      getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+     /* if (toolbar.getNavigationIcon() != null) {
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+      }*/
+
+    }
+
+    Button doneButton = findViewById(R.id.done);
+
+    doneButton.setOnClickListener(v -> startSaveDrawingTask());
+
+
+
+  }
+
+  private void startSaveDrawingTask() {
+    SaveDrawingTask task = new SaveDrawingTask(ResultActivity.this);
+
+    int borderColor;
+    if ((borderColor = getIntent().getIntExtra(CutOut.CUTOUT_EXTRA_BORDER_COLOR, -1)) != -1) {
+      Bitmap image = BitmapUtility.getBorderedBitmap(this.drawView.getDrawingCache(), borderColor, BORDER_SIZE);
+      task.execute(image);
+    } else {
+      task.execute(this.drawView.getDrawingCache());
+    }
   }
 
   /** Start pick image activity with chooser. */
@@ -70,8 +174,13 @@ private ImageCropedAdapter adapter;
       CropImage.ActivityResult result = CropImage.getActivityResult(data);
       if (resultCode == RESULT_OK) {
         //((ImageView) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
-        lists.add(new ImageUriList(result.getUri()));
-        adapter.notifyDataSetChanged();
+       // lists.add(new ImageUriList(result.getUri()));
+      //  adapter.notifyDataSetChanged();
+
+
+          setDrawViewBitmap(result.getUri());
+
+
 
         Toast.makeText(
                 this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG)
@@ -80,5 +189,102 @@ private ImageCropedAdapter adapter;
         Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
       }
     }
+  }
+  private void activateGestureView() {
+    gestureView.getController().getSettings()
+            .setMaxZoom(MAX_ZOOM)
+            .setDoubleTapZoom(-1f) // Falls back to max zoom level
+            .setPanEnabled(true)
+            .setZoomEnabled(true)
+            .setDoubleTapEnabled(true)
+            .setOverscrollDistance(0f, 0f)
+            .setOverzoomFactor(2f);
+  }
+
+  private void deactivateGestureView() {
+    gestureView.getController().getSettings()
+            .setPanEnabled(false)
+            .setZoomEnabled(false)
+            .setDoubleTapEnabled(false);
+  }
+
+  private void initializeActionButtons() {
+    Button autoClearButton = findViewById(R.id.auto_clear_button);
+    Button manualClearButton = findViewById(R.id.manual_clear_button);
+    Button zoomButton = findViewById(R.id.zoom_button);
+
+    autoClearButton.setActivated(false);
+    autoClearButton.setOnClickListener((buttonView) -> {
+      if (!autoClearButton.isActivated()) {
+        drawView.setAction(DrawView.DrawViewAction.AUTO_CLEAR);
+        manualClearSettingsLayout.setVisibility(INVISIBLE);
+        autoClearButton.setActivated(true);
+        manualClearButton.setActivated(false);
+        zoomButton.setActivated(false);
+        deactivateGestureView();
+      }
+    });
+
+    manualClearButton.setActivated(true);
+    drawView.setAction(DrawView.DrawViewAction.MANUAL_CLEAR);
+    manualClearButton.setOnClickListener((buttonView) -> {
+      if (!manualClearButton.isActivated()) {
+        drawView.setAction(DrawView.DrawViewAction.MANUAL_CLEAR);
+        manualClearSettingsLayout.setVisibility(VISIBLE);
+        manualClearButton.setActivated(true);
+        autoClearButton.setActivated(false);
+        zoomButton.setActivated(false);
+        deactivateGestureView();
+      }
+
+    });
+
+    zoomButton.setActivated(false);
+    deactivateGestureView();
+    zoomButton.setOnClickListener((buttonView) -> {
+      if (!zoomButton.isActivated()) {
+        drawView.setAction(DrawView.DrawViewAction.ZOOM);
+        manualClearSettingsLayout.setVisibility(INVISIBLE);
+        zoomButton.setActivated(true);
+        manualClearButton.setActivated(false);
+        autoClearButton.setActivated(false);
+        activateGestureView();
+      }
+
+    });
+  }
+
+  private void setUndoRedo() {
+    Button undoButton = findViewById(R.id.undo);
+    undoButton.setEnabled(false);
+    undoButton.setOnClickListener(v -> undo());
+    Button redoButton = findViewById(R.id.redo);
+    redoButton.setEnabled(false);
+    redoButton.setOnClickListener(v -> redo());
+
+    drawView.setButtons(undoButton, redoButton);
+  }
+
+  public void exitWithError(Exception e) {
+    Intent intent = new Intent();
+    intent.putExtra(CutOut.CUTOUT_EXTRA_RESULT, e);
+    setResult(CutOut.CUTOUT_ACTIVITY_RESULT_ERROR_CODE, intent);
+    finish();
+  }
+
+  private void setDrawViewBitmap(Uri uri) {
+    try {
+      Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+      drawView.setBitmap(bitmap);
+    } catch (IOException e) {
+      exitWithError(e);
+    }
+  }
+  private void undo() {
+    drawView.undo();
+  }
+
+  private void redo() {
+    drawView.redo();
   }
 }
---
+++ app/src/main/java/com/imagecroper/bgremover/SaveDrawingTask.java
@@ -0,0 +1,71 @@
+package com.imagecroper.bgremover;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Pair;
+
+import com.imagecroper.ResultActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+import androidx.annotation.RequiresApi;
+
+import static android.view.View.VISIBLE;
+
+public class SaveDrawingTask extends AsyncTask<Bitmap, Void, Pair<File, Exception>> {
+
+    private static final String SAVED_IMAGE_FORMAT = "png";
+    private static final String SAVED_IMAGE_NAME = "cutout_tmp";
+
+    private final WeakReference<ResultActivity> activityWeakReference;
+
+    public SaveDrawingTask(ResultActivity activity) {
+        this.activityWeakReference = new WeakReference<>(activity);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        activityWeakReference.get().loadingModal.setVisibility(VISIBLE);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected Pair<File, Exception> doInBackground(Bitmap... bitmaps) {
+
+        try {
+            File file = File.createTempFile(SAVED_IMAGE_NAME, SAVED_IMAGE_FORMAT, activityWeakReference.get().getApplicationContext().getCacheDir());
+
+            try (FileOutputStream out = new FileOutputStream(file)) {
+                bitmaps[0].compress(Bitmap.CompressFormat.PNG, 95, out);
+                return new Pair<>(file, null);
+            }
+        } catch (IOException e) {
+            return new Pair<File, Exception>(null, e);
+        }
+    }
+
+    protected void onPostExecute(Pair<File, Exception> result) {
+        super.onPostExecute(result);
+
+        Intent resultIntent = new Intent();
+
+        if (result.first != null) {
+            Uri uri = Uri.fromFile(result.first);
+
+            resultIntent.putExtra(CutOut.CUTOUT_EXTRA_RESULT, uri);
+            activityWeakReference.get().setResult(Activity.RESULT_OK, resultIntent);
+            activityWeakReference.get().finish();
+
+        } else {
+            activityWeakReference.get().exitWithError(result.second);
+        }
+    }
+}
